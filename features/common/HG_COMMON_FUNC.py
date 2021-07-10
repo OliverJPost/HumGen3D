d4cd67d58ddb9b6a8b03410804a2239d3b6c06be
HG3D <support@humgen3d.com>
2021-07-10 12:56:47 +0200

    Make hg_delete also remove images

--- features/common/HG_COMMON_FUNC.py
+++ features/common/HG_COMMON_FUNC.py
@@ -138,10 +138,43 @@ def show_message(self, msg):
 def hg_delete(obj):
     me = obj.data if (obj and obj.type == 'MESH') else None
 
+    images, materials = _get_mats_and_images(obj)    
+    
     bpy.data.objects.remove(obj)
 
     if me and not me.users:
         bpy.data.meshes.remove(me)
+
+    for material in [m for m in materials if m and not m.users]:
+        try:
+            bpy.data.materials.remove(material)
+        except Exception as e:
+            print('Error while deleting material: ', e)
+            pass        
+
+    for image in [i for i in images if i and not i.users]:
+        try:
+            bpy.data.images.remove(image)
+        except Exception as e:
+            print('Error while deleting image: ', e)
+            pass
+
+def _get_mats_and_images(obj):
+    images = []
+    materials = []
+    if obj.type != 'MESH':
+        return [],[]
+    try:       
+        for mat in obj.data.materials:
+            materials.append(mat)
+            nodes = mat.node_tree.nodes
+            for node in [n for n in nodes if n.bl_idname == 'ShaderNodeTexImage']:
+                images.append(node.image)
+    except:
+        raise
+        pass
+    print('returning', images)
+    return list(set(images)), materials
 
 
 #TODO make deepclean data removal, using:
