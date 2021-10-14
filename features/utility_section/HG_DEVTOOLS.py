62bb690b1e4d0a889e007c9794e957306726a548
Oliver J Post <84707941+HG3D@users.noreply.github.com>
2021-10-14 11:44:37 +0200

    Add temporary test operator for hair quality

--- features/utility_section/HG_DEVTOOLS.py
+++ features/utility_section/HG_DEVTOOLS.py
@@ -3,7 +3,7 @@ Operators and functions to be used by the developer and content pack creators
 """
 
 from ... core.HG_SHAPEKEY_CALCULATOR import build_distance_dict
-from ... features.creation_phase.HG_HAIR import convert_to_new_hair_shader
+from ... features.creation_phase.HG_HAIR import convert_to_new_hair_shader, set_hair_quality
 import bpy #type: ignore
 from ... features.common.HG_COMMON_FUNC import find_human
 from ... features.creation_phase.HG_FINISH_CREATION_PHASE import remove_stretch_bones
@@ -18,7 +18,7 @@ class HG_TESTOP(bpy.types.Operator):
     bl_options     = {"UNDO"}
 
     def execute(self,context):
-        build_distance_dict(next(obj for obj in context.selected_objects if obj != context.object), context.object)
+        set_hair_quality(context, 'particle', context.scene.HG3D.batch_hair_quality_particle)
         return {'FINISHED'}
 
 class HG_CONVERT_HAIR_SHADER(bpy.types.Operator):
