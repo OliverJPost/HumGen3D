8a6e4c386876f85af0f0292303db6a1ad3b36ff8
Oliver J Post <84707941+HG3D@users.noreply.github.com>
2021-10-19 11:57:10 +0200

    Hide hair children in background process for performance

--- scripts/batch_generate.py
+++ scripts/batch_generate.py
@@ -12,6 +12,9 @@ settings_dict =  json.loads(sys.argv[4])
 for obj in bpy.data.objects[:]:
     bpy.data.objects.remove(obj)
 
+bpy.context.scene.render.use_simplify = True
+bpy.context.scene.render.simplify_child_particles = 0
+
 bpy.ops.hg3d.quick_generate(**settings_dict)
 
 hg_rig = next(obj for obj in bpy.data.objects if obj.HG.ishuman)
