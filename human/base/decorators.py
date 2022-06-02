781943e1aa145ac9203a6cda9e29ca8bb4b5b898
Oliver J Post <84707941+HG3D@users.noreply.github.com>
2022-06-02 11:35:06 +0200

    Import fix and formatting

--- human/base/decorators.py
+++ human/base/decorators.py
@@ -1,8 +1,7 @@
-
 import functools
 import traceback
 
-from backend.logging import hg_log
+from HumGen3D.backend.logging import hg_log
 
 
 def cached_property(func):
@@ -10,7 +9,6 @@ def cached_property(func):
 
 
 def cached_instance(function):
-
     @functools.wraps(function)
     def wrapper(self, *args, **kwargs):
 
@@ -27,19 +25,23 @@ def cached_instance(function):
 
 def injected_context(func):
     """Replaces keyword argument "context=None" with bpy.context if left at default None value"""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         varnames = func.__code__.co_varnames
-        if 'context' not in varnames:
+        if "context" not in varnames:
             raise TypeError("No argument 'context' in function arguments.")
 
-        context_arg_index = varnames.index('context')
-        context_in_args = len(args) >= (context_arg_index+1)
-        if not context_in_args and 'context' not in kwargs:
-            kwargs['context'] = bpy.context
+        context_arg_index = varnames.index("context")
+        context_in_args = len(args) >= (context_arg_index + 1)
+        if not context_in_args and "context" not in kwargs:
+            kwargs["context"] = bpy.context
 
             hg_log(traceback.extract_stack()[-2])
-            hg_log(f"Argument 'context' for function '{func.__name__}' substituted with bpy.context. It's highly recommended to pass your own context!", level="WARNING")
+            hg_log(
+                f"Argument 'context' for function '{func.__name__}' substituted with bpy.context. It's highly recommended to pass your own context!",
+                level="WARNING",
+            )
 
         return func(*args, **kwargs)
 
