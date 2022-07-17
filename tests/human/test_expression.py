c1a1ebdee216d9842270c42ccdbac0eb54207edc
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-07-17 12:13:19 +0200

    Expression tests

---
+++ tests/human/test_expression.py
@@ -0,0 +1,39 @@
+import os
+import random
+from HumGen3D.tests.fixtures import finalize_phase_human as human, context
+from human.base.exceptions import HumGenException
+
+
+def test_facial_rig(human, context):
+    inital_sk_count = len(human.shape_keys)
+    human.finalize_phase.expression.load_facial_rig(context)
+
+    for bone_name in human.finalize_phase.expression._get_frig_bones():
+        assert not human.pose_bones.get(bone_name).hide
+
+    human.finalize_phase.expression.remove_facial_rig()
+
+    for bone_name in human.finalize_phase.expression._get_frig_bones():
+        assert human.pose_bones.get(bone_name).hide
+
+    try:
+        human.finalize_phase.expression.remove_facial_rig()
+        assert False, "Should throw exception"
+    except HumGenException:
+        assert True
+
+    assert len(human.shape_keys) == inital_sk_count
+
+
+def test_set(human):
+    options = human.finalize_phase.expression.get_options()
+    assert options
+
+    chosen = random.choice(options)
+    human.finalize_phase.expression.set(chosen)
+    sk_name, _ = os.path.splitext(os.path.basename(chosen))
+
+    sk = human.shape_keys.get(sk_name)
+    assert sk
+    assert sk.value
+    assert not sk.mute
