fce36981ab58ac0e7e8156b0c6bb0133cda851d2
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-09-12 19:25:00 +0200

    Hide all objects except body during slider modal

--- human/length/armature_update.py
+++ human/length/armature_update.py
@@ -6,12 +6,13 @@ class HG3D_OT_SLIDER_SUBSCRIBE(bpy.types.Operator):
     bl_idname = "hg3d.slider_subscribe"
     bl_label = ""
     stop: bpy.props.BoolProperty()
+    bl_options = {"UNDO"}
 
     def modal(self, context, event):
         if self.stop:
             context.scene.HG3D.slider_is_dragging = False
-            human = Human.from_existing(context.object)
-            human.length.correct_armature(context)
+            self.human.hide_set(False)
+            self.human.length.correct_armature(context)
             return {"FINISHED"}
         if event.value == "RELEASE":
             self.stop = True
@@ -20,5 +21,9 @@ class HG3D_OT_SLIDER_SUBSCRIBE(bpy.types.Operator):
 
     def invoke(self, context, event):
         self.stop = False
+        self.human = Human.from_existing(context.object)
+        self.human.hide_set(True)
+        self.human.body_obj.hide_set(False)
+        self.human.body_obj.hide_viewport = False
         context.window_manager.modal_handler_add(self)
         return {"RUNNING_MODAL"}
