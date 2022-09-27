8aa66e8e8ee605c772818d8628ff28c6ae9f1da4
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-09-27 16:02:20 +0200

    auto_classes refactor
    No functional changes

--- backend/auto_classes.py
+++ backend/auto_classes.py
@@ -15,34 +15,34 @@ YIELD_LATER = (
     "HG_OBJECT_PROPS",
 )
 
+# There are more, but I'm not using them
+BPY_CLASSES = (
+    bpy.types.Operator,
+    bpy.types.PropertyGroup,
+    bpy.types.Panel,
+    bpy.types.AddonPreferences,
+    bpy.types.Header,
+    bpy.types.Menu,
+    bpy.types.UIList,
+)
+
 
 def _get_bpy_classes():
     dir_path = os.path.dirname(os.path.abspath(HumGen3D.__file__))
 
-    # There are more, but I'm not using them
-    bpy_classes = (
-        bpy.types.Operator,
-        bpy.types.PropertyGroup,
-        bpy.types.Panel,
-        bpy.types.AddonPreferences,
-        bpy.types.Header,
-        bpy.types.Menu,
-        bpy.types.UIList,
-    )
-
     py_files = get_python_files_from_dir(dir_path)
 
-    yielded = []
+    already_yielded = []
     for root, filename in py_files:
         module = import_pyfile_as_module(dir_path, root, filename)
 
         yield_later = []
         yield_last = []
         for name, obj in inspect.getmembers(module):
-            if not inspect.isclass(obj) or not issubclass(obj, bpy_classes):
+            if not inspect.isclass(obj) or not issubclass(obj, BPY_CLASSES):
                 continue
             # Skip classes that have been yielded previously
-            if name in yielded:
+            if name in already_yielded:
                 continue
 
             # Check if the class is actually from the HumGen3D module
@@ -53,26 +53,16 @@ def _get_bpy_classes():
                 yield_last.append(obj)
                 continue
 
-            if obj.__name__ in YIELD_LATER:
-                yield_later.append(obj)
-                continue
-
             # Wait with yielding UI classes that depend on parent
-            if hasattr(obj, "bl_parent_id"):
+            if obj.__name__ in YIELD_LATER or hasattr(obj, "bl_parent_id"):
                 yield_later.append(obj)
                 continue
 
-            yielded.append(name)
+            already_yielded.append(name)
             yield obj
 
         yield from yield_later
         yield from yield_last
-
-    from HumGen3D.user_interface.documentation.tutorial_operator import (
-        tutorial_operator,
-    )
-
-    yield tutorial_operator.HG_DRAW_PANEL
 
 
 def import_pyfile_as_module(dir_path, root, filename):
@@ -80,16 +70,16 @@ def import_pyfile_as_module(dir_path, root, filename):
     rel_path_split = os.path.normpath(os.path.relpath(abspath, dir_path)).split(os.sep)
     module_import_path = ".".join(rel_path_split)
 
-    mod = __import__(
+    module = __import__(
         ".".join(["HumGen3D", module_import_path[:-3]]),
         fromlist=[module_import_path[:-3]],
     )
 
-    return mod
+    return module
 
 
 def get_python_files_from_dir(dir_path):
-    skip_dirs = (".vscode", ".mypy", ".git", "tutorial_operator", "tests")
+    skip_dirs = (".vscode", ".mypy", ".git", "tests")
     py_files = []
     for root, _, files in os.walk(dir_path):
         if any(dir in root for dir in skip_dirs):
