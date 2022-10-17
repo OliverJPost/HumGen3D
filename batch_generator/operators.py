29cce54bc25fa7df3f4539274018b31f01d1f459
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-10-17 10:20:18 +0200

    Give timing notification for batch generator

--- batch_generator/operators.py
+++ batch_generator/operators.py
@@ -6,11 +6,11 @@ import os
 import time
 
 import bpy
-from HumGen3D.backend import hg_log
 from HumGen3D.backend.preferences.preference_func import get_addon_root
 from HumGen3D.human.base.collections import add_to_collection
 from HumGen3D.human.base.render import set_eevee_ao_and_strip
 from HumGen3D.human.human import Human
+from HumGen3D.user_interface.documentation.feedback_func import ShowMessageBox
 
 from .batch_functions import get_batch_marker_list, has_associated_human
 from .generator import BatchHumanGenerator
@@ -123,7 +123,8 @@ class HG_BATCH_GENERATE(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     run_immediately: bpy.props.BoolProperty(default=False)
-    show_time_dialog: bpy.props.BoolProperty(default=False)
+    do_timing_run_first: bpy.props.BoolProperty(default=False)
+    timing_run_marker: bpy.props.StringProperty()
 
     def invoke(self, context, event):
         batch_sett = context.scene.HG3D.batch
@@ -132,33 +133,70 @@ class HG_BATCH_GENERATE(bpy.types.Operator):
             filter(has_associated_human, self.generate_queue)
         )
 
-        if self.show_time_dialog:
-            self.show_time_dialog = False
-            return self.execute(context)
-
-        if len(self.generate_queue) >= 3:
-            self.show_time_dialog = True
-
-        if self.run_immediately or not markers_with_associated_human:
-            set_eevee_ao_and_strip(context)
-            self.generator = BatchHumanGenerator()
-            set_generator_settings(self.generator, batch_sett)
-
-            if self.show_time_dialog:
-                self.execute_first(context)
-                bpy.app.timers.register(
-                    lambda: bpy.ops.hg3d.generate(show_time_dialog=True),
-                    first_interval=0,
-                )
-                return {"CANCELLED"}
-            else:
-                return self.execute(context)
-        else:
+        # Show a dialog asking the user if markers should be overwritten
+        if not self.run_immediately and markers_with_associated_human:
             self._show_dialog_to_confirm_deleting_humans(context)
             return {"CANCELLED"}
 
-    def execute_first(self, context):
-        marker = self.generate_queue[0]
+        set_eevee_ao_and_strip(context)
+        self.generator = BatchHumanGenerator()
+        set_generator_settings(self.generator, batch_sett)
+
+        # Generate the remaining humans from the timing run
+        if self.do_timing_run_first:
+            self.do_timing_run_first = False
+            return self.execute(context)
+
+        if len(self.generate_queue) < 3:
+            return self.execute(context)
+        else:
+            t = time.time()
+            # Generate just one human to get timing
+            marker = self.generate_queue[0]
+            self._generate_human_for_marker(context, marker)
+            t_delta = time.time() - t
+
+            # Show message box with estimated time needed
+            len_markers = len(self.generate_queue)
+            # For some reason it takes almost twice as long as expected from timing.
+            t_total = (len_markers - 1) * t_delta * 2
+            t_min = int(t_total // 60)
+            t_sec = int(t_total % 60)
+            ShowMessageBox(
+                (
+                    "Generating humans....\n"
+                    + f"All {len_markers} humans expected to take {t_min}m {t_sec}s."
+                )
+            )
+            bpy.app.timers.register(
+                lambda: bpy.ops.hg3d.generate(
+                    do_timing_run_first=True,
+                    run_immediately=True,
+                    timing_run_marker=marker.name,
+                ),
+                first_interval=1,
+            )
+            return {"FINISHED"}
+
+    def execute(self, context):
+        # Call invoke if it wasn't run because of the timer
+        if not hasattr(self, "generate_queue"):
+            self.invoke(context, None)
+            # Skip first marker since it was generated during intital run
+            marker_obj = bpy.data.objects.get(self.timing_run_marker)
+            self.generate_queue.remove(marker_obj)
+
+        for marker in self.generate_queue:
+            self._generate_human_for_marker(context, marker)
+
+        ShowMessageBox("Batch Generation Completed!")
+        return {"FINISHED"}
+
+    def _generate_human_for_marker(self, context, marker):
+        """Generate a new human at the position of this batch marker.
+
+        Uses self.generator
+        """
         pose_type = marker["hg_batch_marker"]
 
         if has_associated_human(marker):
@@ -171,22 +209,6 @@ class HG_BATCH_GENERATE(bpy.types.Operator):
         human.rotation_euler = marker.rotation_euler
         marker["associated_human"] = human.rig_obj
 
-    def execute(self, context):
-        for marker in self.generate_queue:
-            pose_type = marker["hg_batch_marker"]
-
-            if has_associated_human(marker):
-                old_human = Human.from_existing(marker["associated_human"])
-                old_human.delete()
-
-            human = self.generator.generate_human(context, pose_type)
-
-            human.location = marker.location
-            human.rotation_euler = marker.rotation_euler
-            marker["associated_human"] = human.rig_obj
-
-        return {"FINISHED"}
-
     def _show_dialog_to_confirm_deleting_humans(self, context):  # noqa CCE001
         generate_queue = self.generate_queue
 
@@ -211,155 +233,3 @@ class HG_BATCH_GENERATE(bpy.types.Operator):
             return
 
         context.window_manager.popup_menu(draw, title="This will delete these humans:")
-
-
-class HG_BATCH_GENERATE_MODAL(bpy.types.Operator):
-    bl_idname = "hg3d.generate_modal"
-    bl_label = "Generate"
-    bl_description = "Generates specified amount of humans"
-    bl_options = {"REGISTER", "UNDO"}
-
-    run_immediately: bpy.props.BoolProperty(default=False)
-
-    def __init__(self):
-        self.human_idx = 0
-        self.generate_queue = get_batch_marker_list(bpy.context)
-        self.finish_modal = False
-        self.timer = None
-        self.start_time = time.time()
-
-    def invoke(self, context, event):
-        batch_sett = context.scene.HG3D.batch
-
-        markers_with_associated_human = list(
-            filter(has_associated_human, self.generate_queue)
-        )
-
-        if self.run_immediately or not markers_with_associated_human:
-            self._initiate_modal(context, batch_sett)
-            set_eevee_ao_and_strip(context)
-            self.generator = BatchHumanGenerator()
-            set_generator_settings(self.generator, batch_sett)
-            return {"RUNNING_MODAL"}
-        else:
-            self._show_dialog_to_confirm_deleting_humans(context)
-            return {"CANCELLED"}
-
-    def _initiate_modal(self, context, batch_sett):  # noqa
-        wm = context.window_manager
-        wm.modal_handler_add(self)
-
-        batch_sett.progress = 0
-
-        self.human_idx = 0
-        self.timer = wm.event_timer_add(0.01, window=context.window)
-
-        batch_sett.idx = 1
-        context.workspace.status_text_set(status_text_callback)
-        context.area.tag_redraw()
-
-    def _show_dialog_to_confirm_deleting_humans(self, context):  # noqa CCE001
-        generate_queue = self.generate_queue
-
-        def draw(self, context):
-            layout = self.layout
-
-            nonlocal generate_queue
-
-            for i, marker in enumerate(filter(has_associated_human, generate_queue)):
-                layout.label(text=marker["associated_human"].name)
-
-                if i > 9:
-                    layout.label(text=f"+ {len(generate_queue) - 10} more")
-                    break
-
-            layout.separator()
-
-            layout.operator_context = "INVOKE_DEFAULT"
-            layout.operator(
-                "hg3d.generate", text="Generate anyway"
-            ).run_immediately = True
-            return
-
-        context.window_manager.popup_menu(draw, title="This will delete these humans:")
-
-    def modal(self, context, event):  # noqa CFQ004
-        """Event handling."""
-        batch_sett = context.scene.HG3D.batch
-
-        if self.finish_modal:
-            context.area.tag_redraw()
-            context.workspace.status_text_set(text=None)
-
-            batch_sett.idx = 0
-
-            hg_log(
-                "Batch modal total running time: ",
-                round(time.time() - self.start_time, 2),
-                "s",
-            )
-
-            return {"FINISHED"}
-
-        elif event.type in ["ESC"]:
-            self._cancel(batch_sett, context)
-
-            return {"RUNNING_MODAL"}
-
-        elif event.type == "TIMER":
-
-            # Check if all humans in the list are already generated
-            if self.human_idx == len(self.generate_queue):
-                self.finish_modal = True
-                return {"RUNNING_MODAL"}
-
-            current_marker = self.generate_queue[self.human_idx]
-
-            pose_type = current_marker["hg_batch_marker"]
-
-            human = self.generator.generate_human(context, pose_type)
-
-            human.location = current_marker.location
-            human.rotation_euler = current_marker.rotation_euler
-            current_marker["associated_human"] = human.rig_obj
-
-            self.human_idx += 1
-
-            if self.human_idx > 0:
-                progress = self.human_idx / (len(self.generate_queue))
-                batch_sett.progress = int(progress * 100)
-
-            batch_sett.idx += 1
-
-            context.workspace.status_text_set(status_text_callback)
-
-            return {"RUNNING_MODAL"}
-
-        else:
-            return {"RUNNING_MODAL"}
-
-    def _delete_old_associated_human(self, marker):
-        associated_rig = marker["associated_human"]
-        human = Human.from_existing(associated_rig)
-        human.delete()
-
-    def _cancel(self, batch_sett, context):
-        hg_log("Batch modal is cancelling")
-        batch_sett.progress = batch_sett.progress + (100 - batch_sett.progress) / 2.0
-
-        self.finish_modal = True
-        context.workspace.status_text_set(status_text_callback)
-        return {"CANCELLED"}
-
-
-class HG_RESET_BATCH_OPERATOR(bpy.types.Operator):
-    """Operator for testing bits of code."""
-
-    bl_idname = "hg3d.reset_batch_operator"
-    bl_label = "Reset batch operator"
-    bl_description = "If an error occured during batch creation, use this to get the purple button back"  # noqa E501
-    bl_options = {"UNDO"}
-
-    def execute(self, context):
-        context.scene.HG3D.batch_idx = 0
-        return {"FINISHED"}
