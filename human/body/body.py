797e77be65647fd3ced25bdfd45bd0743748f95c
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-11-04 15:36:01 +0100

    Fix docstrings

--- human/body/body.py
+++ human/body/body.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2022 Oliver J. Post & Alexander Lashko - GNU GPL V3.0, see LICENSE
+"""Module containing subpart of Human class to edit body proportions of the human."""
 
 import random
 from typing import TYPE_CHECKING, Union, cast
 
-from HumGen3D.common.type_aliases import C
 from HumGen3D.common.decorators import injected_context
+from HumGen3D.common.type_aliases import C
 
 if TYPE_CHECKING:
     from HumGen3D import Human
@@ -12,12 +13,39 @@ if TYPE_CHECKING:
 
 
 class BodySettings:
+    """Class to edit body proportions of the human.
+
+    The body proportions are changed by changing the values of the LiveKeys stored
+    in the folder `livekeys/body_proportions`. These are accessible in the API under
+    `human.body.keys`.
+
+    Note: By default Human Generator does not come with shape keys for changing body
+    proportions, but either by users converting them or by new 3rd party packs adding
+    them you can expect shape keys too.
+
+    For more info on LiveKeys see [[LiveKeys]].
+    """
+
     def __init__(self, human: "Human") -> None:
-        """Initiate instance for modifying body of human."""
+        """Initiate instance for modifying body of human.
+
+        Args:
+            human (Human): Human instance.
+        """
         self._human: "Human" = human
 
     @property
     def keys(self) -> list[Union["ShapeKeyItem", "LiveKeyItem"]]:
+        """Access ShapeKeyItems and LiveKeyItems for changing body proportions.
+
+        Gets all LiveKeyItems and ShapeKeyItems from the global Human Generator
+        key collection (`Human.keys.all_keys`) that belong to the body proportion
+        category.
+
+        Returns:
+            list[Union[ShapeKeyItem, LiveKeyItem]]: List of all keys that belong to the
+                body proportions category.
+        """
         return cast(
             list[Union["ShapeKeyItem", "LiveKeyItem"]],
             self._human.keys.filtered("body_proportions"),
@@ -25,10 +53,13 @@ class BodySettings:
 
     @injected_context
     def randomize(self, context: C = None) -> None:
-        """Randomizes the body type sliders of the active human.
+        """Randomizes the values of the body keys of this human.
+
+        Uses a random value retreived from a normal distrubition with mean 0 and sigma
+        of 0.5.
 
         Args:
-            hg_rig (Object): HumGen armature
+            context (C): Blender context
         """
         for key in self.keys:
             if key.name == "skinny":
