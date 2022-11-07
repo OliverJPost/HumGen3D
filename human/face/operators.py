4b1f4b9e3a0c9899bb06aab7300a0fab117835a0
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-11-07 16:16:40 +0100

    Fix/add docstrings

--- human/face/operators.py
+++ human/face/operators.py
@@ -1,19 +1,14 @@
 # Copyright (c) 2022 Oliver J. Post & Alexander Lashko - GNU GPL V3.0, see LICENSE
 
+"""Implements Blender operators related to facial proportions."""
+
 import bpy
 
 from ..human import Human
 
 
 class HG_RESET_FACE(bpy.types.Operator):
-    """Resets all face deformation values to 0
-
-    Operator type:
-        Prop setter
-
-    Prereq:
-        Active object is part of HumGen human
-    """
+    """Resets all face deformation values to 0"""
 
     bl_idname = "hg3d.resetface"
     bl_label = "Reset face"
