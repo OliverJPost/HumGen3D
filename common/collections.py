dd584054b0d01353941c77105c597300f6eb40f1
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-11-09 13:16:26 +0100

    Refactor collections function

--- common/collections.py
+++ common/collections.py
@@ -10,25 +10,18 @@ def add_to_collection(
     """Adds the giver object toa colleciton. By default added to HumGen collection.
 
     Args:
+        context(bpy.types.Context): Blender context
         obj (Object): object to add to collection
         collection_name (str, optional): Name of collection. Defaults to 'HumGen'.
 
     Returns:
         bpy.types.Collection: Collection the object was added to
     """
-    collection = bpy.data.collections.get(collection_name)
+    collection = bpy.data.collections.get(
+        collection_name, _new_collection(context, collection_name)
+    )
 
-    if not collection:
-        collection = bpy.data.collections.new(name=collection_name)
-        if collection_name == "HG Batch Markers":
-            hg_collection = bpy.data.collections.get("HumGen")
-            if not hg_collection:
-                hg_collection = bpy.data.collections.new(name="HumGen")
-                context.scene.collection.children.link(hg_collection)
-            hg_collection.children.link(collection)
-        else:
-            context.scene.collection.children.link(collection)
-
+    # Unlink object from old collection
     if context.scene.collection.objects.get(obj.name):
         context.scene.collection.objects.unlink(obj)
     else:
@@ -36,4 +29,19 @@ def add_to_collection(
 
     collection.objects.link(obj)
 
+    return collection  # type: ignore [return-value]
+
+
+def _new_collection(
+    context: bpy.types.Context, collection_name: str
+) -> bpy.types.Collection:
+    collection = bpy.data.collections.new(name=collection_name)
+    if collection_name == "HG Batch Markers":
+        hg_collection = bpy.data.collections.get("HumGen")
+        if not hg_collection:
+            hg_collection = bpy.data.collections.new(name="HumGen")
+            context.scene.collection.children.link(hg_collection)
+        hg_collection.children.link(collection)
+    else:
+        context.scene.collection.children.link(collection)
     return collection
