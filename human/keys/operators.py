f8550df956dc075439bab5f72c2cad42603e4a29
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2022-11-24 20:50:35 +0100

    Notification for lk to sk

--- human/keys/operators.py
+++ human/keys/operators.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2022 Oliver J. Post & Alexander Lashko - GNU GPL V3.0, see LICENSE
 
 import bpy
-
 from HumGen3D.human.human import Human
+from HumGen3D.user_interface.documentation.feedback_func import ShowMessageBox
 
 
 class HG_OT_LIVETOSHAPEKEY(bpy.types.Operator):
@@ -17,5 +17,9 @@ class HG_OT_LIVETOSHAPEKEY(bpy.types.Operator):
         human = Human.from_existing(context.object)
 
         human.keys[self.livekey_name].to_shapekey()
-
+        ShowMessageBox(
+            "Live key converted to shape key. This slider is still the livekey, but the shape key is now found in Blender's shape key menu.",
+            "Success",
+            "INFO",
+        )
         return {"FINISHED"}
