46c152f78a90e84bd5a7dbc83a2920e75c27aa9d
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-02-26 01:17:19 +0100

    Remove old documentation link

--- user_interface/documentation/tips_and_suggestions/main_ui_tips_and_suggestions.py
+++ user_interface/documentation/tips_and_suggestions/main_ui_tips_and_suggestions.py
@@ -8,12 +8,4 @@ from HumGen3D.user_interface.documentation.tips_and_suggestions.tip_baseclasses 
 
 
 def get_main_ui_tips_from_context(context, sett, human):
-    yield main_tutorial
-
-
-main_tutorial = Tip(
-    "Documentation",
-    "You can find the HG documentation here:",
-    icon="HELP",
-    operator=URLOperator("Open website", "https://help.humgen3d.com"),
-)
+    pass
