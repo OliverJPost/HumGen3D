c769f754994e2d6ef28645fb75277e37bafe1817
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-03-07 00:43:38 +0100

    Add clothing tip

--- user_interface/documentation/tips_and_suggestions/main_ui_tips_and_suggestions.py
+++ user_interface/documentation/tips_and_suggestions/main_ui_tips_and_suggestions.py
@@ -1,11 +1,38 @@
 # Copyright (c) 2022 Oliver J. Post & Alexander Lashko - GNU GPL V3.0, see LICENSE
-
+from enum import Enum
 
 from HumGen3D.user_interface.documentation.tips_and_suggestions.tip_baseclasses import (
     Tip,
     URLOperator,
 )
 
+# String enum of possible active sections
+class ActiveSection(str, Enum):
+    """Enum of possible active sections."""
+
+    BODY = "body"
+    AGE = "age"
+    FACE = "face"
+    HEIGHT = "height"
+    SKIN = "skin"
+    HAIR = "hair"
+    CLOTHING = "clothing"
+    POSE = "pose"
+    EXPRESSION = "expression"
+
+
+CLOTHING_MATERIAL_MENU_TIP = Tip(
+    title="Clothing Materials",
+    text="""By selecting a clothing object, the Human Generator
+interface will change to the clothing material menu.
+
+This menu allows you to change the colors of the clothing
+and also add patterns to it.""",
+)
+
 
 def get_main_ui_tips_from_context(context, sett, human):
-    pass
+    active_tab = sett.ui.phase
+
+    if active_tab == ActiveSection.CLOTHING and human.clothing.outfit.objects:
+        yield CLOTHING_MATERIAL_MENU_TIP
