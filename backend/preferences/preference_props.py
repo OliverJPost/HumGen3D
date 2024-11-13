772378161e56aeef6fc1967577d6c421aaef3c38
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2024-11-13 14:18:48 +0100

    Update hardcoded dev path

--- backend/preferences/preference_props.py
+++ backend/preferences/preference_props.py
@@ -93,7 +93,7 @@ class HGPreferenceBackend:
         else:
             # Return hard coded path if developing from my computer, prevents having to remove path for every release.
             if getpass.getuser() == "ole":
-                return "/Users/ole/Library/Mobile Documents/com~apple~CloudDocs/Areas/Human Generator/Content"
+                return "/Users/ole/Library/Mobile Documents/com~apple~CloudDocs/Areas/Human Generator/Content/"
             else:
                 return ""
 
