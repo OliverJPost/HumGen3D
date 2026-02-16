3b3e3577852fca6baae08f6558d853c4107ae2e0
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2026-02-16 11:27:35 +0100

    Bump to 4.0.29

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ bl_info = {
     "author": "OliverJPost",
     "description": "Human Generator allows you to generate humans including clothing, poses and emotions",  # noqa
     "blender": (3, 2, 0),
-    "version": (4, 0, 28),  # RELEASE update version number
+    "version": (4, 0, 29),  # RELEASE update version number
     "location": "Add-On Sidepanel > HumGen",
     "wiki_url": "https://humgen3d.com",
     "tracker_url": "https://humgen3d.com",
